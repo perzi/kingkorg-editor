9f9a92af5eda8a4ecd5917530734c24e0ee1eeae
Per Zimmerman <per.zimmerman@gmail.com>
2015-09-08 13:18:33 +0200

    Added kingkorg component for simple connection

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,18 +1,32 @@
 //import React from 'react';
 import React                    from 'react/addons';
+import KingKORG                 from 'components/kingkorg';
 
 class Intro extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state                    = {
+    this.state = {
     };
+  }
+
+  handleMidiMessage(evt) {
+    if (evt.data[0] !== 0xF8) {
+      console.log("MIDI IN!", evt.data);
+    }
+  }
+
+  handleKbdMessage(evt) {
+    if (evt.data[0] !== 0xF8) { // internal clock
+      console.log("KBD IN!", evt.data);
+    }
   }
 
   render() {
     return (
       <div>
           <h2>Intro</h2>
+          <KingKORG onMidiMessage={this.handleMidiMessage} onKbdMessage={this.handleKbdMessage}/>
       </div>
     );
   }
