ef78a091748323804a82576ad208954871b81a8d
Per Zimmerman <per.zimmerman@gmail.com>
2016-01-26 15:32:20 +0100

    Use payload for actions and parse sysex in action creator

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -10,62 +10,61 @@ export const LOAD_CURRENT_PROGRAM             = 'LOAD_CURRENT_PROGRAM';
  * action creators
  */
 export function setCurrentProgramName(name) {
-  return { type: SET_CURRENT_PROGRAM_NAME, name }
+  return {
+    type: SET_CURRENT_PROGRAM_NAME,
+    payload: {
+      name
+    }
+  }
 }
 
 export function updateCurrentProgramParam(index, value) {
-  return { type: CHANGE_CURRENT_PROGRAM_PARAMETER, index, value }
+  return {
+    type: CHANGE_CURRENT_PROGRAM_PARAMETER,
+    payload: {
+      index,
+      value
+    }
+  }
 }
 
 export function loadCurrentProgram(sysexData) {
-  return { type: LOAD_CURRENT_PROGRAM, sysexData }
+
+  let program = convertSysexDataToProgram(sysexData)
+
+  return {
+    type: LOAD_CURRENT_PROGRAM,
+    payload: program
+  }
 }
 
+function convertSysexDataToProgram(sysexData) {
+  let name = "";
 
+  // remove first bytes and the last one
+  let programData = sysexData.slice(7, sysexData.length - 1);
 
-//
-//   setName(name) {
-//     this.dispatch(Immutable.fromJS({
-//       name: name
-//     }));
-//   }
-//
-//   updateParam(offset, value) {
-//     this.dispatch({
-//       index: offset,
-//       value: value
-//     });
-//   }
-//
-//   fromData(sysexData) {
-//
-//     var name = "";
-//
-//     // remove first bytes and the last one
-//     let programData = sysexData.slice(7, sysexData.length - 1);
-//
-//     let data = [];
-//
-//     for (let i = 0; i < programData.length; i++) {
-//
-//       let r = i % 8; // get order
-//
-//       if (r !== 0) {
-//         let highbits = programData[Math.floor(i / 8) * 8];
-//         let highbit = (highbits << (8 - r)) & 0b10000000;
-//         let value = highbit | programData[i];
-//
-//         data.push(value);
-//       }
-//     }
-//
-//     for (let i = 0; i < 12; i++) {
-//       name = name + String.fromCharCode(data[i]);
-//     }
-//
-//     this.dispatch(Immutable.fromJS({
-//       data: data,
-//       name: name
-//     }));
-//   }
-// }
+  let data = [];
+
+  for (let i = 0; i < programData.length; i++) {
+
+    let r = i % 8; // get order
+
+    if (r !== 0) {
+      let highbits = programData[Math.floor(i / 8) * 8];
+      let highbit = (highbits << (8 - r)) & 0b10000000;
+      let value = highbit | programData[i];
+
+      data.push(value);
+    }
+  }
+
+  for (let i = 0; i < 12; i++) {
+    name = name + String.fromCharCode(data[i]);
+  }
+
+  return {
+    name,
+    data
+  }
+}
--- src/reducers/currentprogram.js
+++ src/reducers/currentprogram.js
@@ -7,12 +7,12 @@ const initialState = {
 
 
 export default function currentProgram(state = initialState, action) {
+
   switch (action.type) {
     case SET_CURRENT_PROGRAM_NAME:
       return Object.assign({}, state,
         {
-          name: action.name,
-          data: state.data
+          name: action.payload.name,
         }
       );
 
@@ -21,46 +21,19 @@ export default function currentProgram(state = initialState, action) {
       // TODO: enable update of more than one byte
       return Object.assign({}, state,
         {
-          name: state.name,
           data: [
-            ...state.data.slice(0, action.index),
-            action.value,
-            ...state.data.slice(action.index + 1)
+            ...state.data.slice(0, action.payload.index),
+            action.payload.value,
+            ...state.data.slice(action.payload.index + 1)
           ]
         }
       );
 
     case LOAD_CURRENT_PROGRAM:
-      var name = "";
-
-      // remove first bytes and the last one
-      let programData = action.sysexData.slice(7, action.sysexData.length - 1);
-
-      let data = [];
-
-      for (let i = 0; i < programData.length; i++) {
-
-        let r = i % 8; // get order
-
-        if (r !== 0) {
-          let highbits = programData[Math.floor(i / 8) * 8];
-          let highbit = (highbits << (8 - r)) & 0b10000000;
-          let value = highbit | programData[i];
-
-          data.push(value);
-        }
-      }
-
-      for (let i = 0; i < 12; i++) {
-        name = name + String.fromCharCode(data[i]);
-      }
-
-      return Object.assign({}, state,
-        {
-          name: name,
-          data: data
-        }
-      );
+      return {
+        name: action.payload.name,
+        data: action.payload.data
+      };
 
     default:
       return state
