af5bdc3cec0062d1388f340ecd389a722ecee7db
Per Zimmerman <per.zimmerman@gmail.com>
2016-02-09 10:50:39 +0100

    Add className argument to component helper

--- src/util/component-helpers.js
+++ src/util/component-helpers.js
@@ -1,10 +1,11 @@
-export function getControlParameter({ parameter, data }, id, type) {
+export function getControlParameter({ parameter, data }, id, type, className) {
   let constrolParameter = parameter.getParameter(id);
 
   return {
     data: data,
     parameter: constrolParameter,
     id,
-    type
+    type,
+    className
   }
 }
