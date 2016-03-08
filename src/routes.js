bb05a86402fb351bcc01f201a862bd23a5ce9654
Per Zimmerman <per.zimmerman@gmail.com>
2016-03-08 23:28:27 +0100

    Use historyHash for Router

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React                 from 'react';
-import { Router, Route, IndexRoute } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import App                   from 'components/App';
 import CurrentProgram        from 'components/CurrentProgram';
 import Sysex                 from 'components/Sysex';
 
 const routes = (
-  <Router>
+  <Router history={hashHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={CurrentProgram} />
       <Route path='currentprogram' component={CurrentProgram}/>
