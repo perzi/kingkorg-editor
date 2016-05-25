78db8656e130790a2c0a6a3a8d246edfb12d2a45
Per Zimmerman <per.zimmerman@gmail.com>
2016-05-25 19:49:51 +0200

    Use browserhistory

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React                 from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App                   from 'components/App';
 import CurrentProgram        from 'components/CurrentProgram';
 import Sysex                 from 'components/Sysex';
 
 const routes = (
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={CurrentProgram} />
       <Route path='currentprogram' component={CurrentProgram}/>
